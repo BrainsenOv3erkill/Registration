355584504fb946e43f9a56ff34fd24e86583a494
BrainsenOv3erkill <165007784+BrainsenOv3erkill@users.noreply.github.com>
2024-05-16 15:34:05 +0200

    Access granted

---
+++ server.js
@@ -0,0 +1,47 @@
+
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const fs = require("fs");  
+
+
+
+
+app.use(express.urlencoded({ extended: true }));
+
+
+ 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html" );
+
+});
+
+
+
+app.post("/submit", (req, res) => {
+
+  let datenSpeicher = []
+    datenSpeicher.push(req.body.name)
+    datenSpeicher.push(req.body.email)
+    datenSpeicher.push(req.body.password)
+
+
+     fs.appendFile(__dirname + "/userdaten.txt", datenSpeicher.join("\n"), (err) => {
+            if (err) {
+              res.status(500).send('Fehler beim Speichern der Daten');
+              return;
+            }
+            console.log('Daten wurden zur Datei hinzugefügt.');
+            res.send('Daten wurden erfolgreich empfangen!');
+     })
+
+//    console.log(req.body)  
+//    res.send(201);
+});
+
+app.listen(PORT, () => {
+    console.log(`Der Server wurde gestartet auf Port: ${PORT}`);
+
+});
